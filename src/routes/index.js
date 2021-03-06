9fa960c83d7e8ee50d53b87e3438c5ae8725147d
Alexander Hoinville <ahoin001@gmail.com>
2021-03-05 19:39:47 -0500

    Changed message displayed visiting back end

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send('JDEOUFBOEOB');
+  res.send('Back end up and running!');
 });
 
 module.exports = router;
