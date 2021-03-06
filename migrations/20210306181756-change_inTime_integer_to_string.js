b40d92544003cc2b32c35a5c0c6b3be5865ddc27
Alexander Hoinville <ahoin001@gmail.com>
2021-03-06 14:20:54 -0500

    Removed unnedded migration

--- migrations/20210306181756-change_inTime_integer_to_string.js
+++ migrations/20210306181756-change_inTime_integer_to_string.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.changeColumn('Subtitles', 'outTime', {
+      queryInterface.changeColumn('Subtitles', 'inTime', {
         type: Sequelize.STRING,
         allowNull: false
       }),
